ee6e6f997d5a3cc13b5a814f139afdadd49d59e2
yklee <gerion@gmail.com>
2023-09-02 10:15:59 +0900

    first commit

---
+++ src/test/kotlin/com/jake/simpleboard/SampleTest.kt
@@ -0,0 +1,4 @@
+package com.jake.simpleboard
+
+class SampleTest {
+}
---
+++ src/test/kotlin/com/jake/simpleboard/SimpleBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jake.simpleboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
