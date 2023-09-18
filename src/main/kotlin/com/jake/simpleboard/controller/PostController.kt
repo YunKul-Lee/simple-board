5821838c18cb81b00a1d7efeeab14b350f8634bb
yklee <gerion@gmail.com>
2023-09-18 23:03:12 +0900

    feat: Design AP and entity

--- src/main/kotlin/com/jake/simpleboard/controller/PostController.kt
+++ src/main/kotlin/com/jake/simpleboard/controller/PostController.kt
@@ -1,21 +1,28 @@
 package com.jake.simpleboard.controller
 
 import com.jake.simpleboard.controller.dto.PostCreateRequest
+import com.jake.simpleboard.controller.dto.PostDetailResponse
+import com.jake.simpleboard.controller.dto.PostSearchRequest
+import com.jake.simpleboard.controller.dto.PostSummaryResponse
 import com.jake.simpleboard.controller.dto.PostUpdateRequest
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 class PostController {
 
     @PostMapping("/posts")
     fun createPost(
-        @RequestBody postCreateRequest: PostCreateRequest
+        @RequestBody postCreateRequest: PostCreateRequest,
     ): Long {
         return 1L
     }
@@ -23,18 +30,35 @@ class PostController {
     @PutMapping("/posts/{id}")
     fun updatePost(
         @PathVariable id: Long,
-        @RequestBody postUpdateRequest: PostUpdateRequest
+        @RequestBody postUpdateRequest: PostUpdateRequest,
     ): Long {
-        return id;
+        return id
     }
 
     @DeleteMapping("/posts/{id}")
     fun deletePost(
         @PathVariable id: Long,
-        @RequestParam createdBy: String
+        @RequestParam createdBy: String,
     ): Long {
         println(createdBy)
-        return id;
+        return id
     }
 
+    @GetMapping("/posts/{id}")
+    fun getPost(
+        @PathVariable id: Long,
+    ): PostDetailResponse {
+        return PostDetailResponse(1L, "title", "content", "jake", LocalDateTime.now().toString())
+    }
+
+    @GetMapping("/posts")
+    fun getPosts(
+        pageable: Pageable,
+        postSearchRequest: PostSearchRequest,
+    ): Page<PostSummaryResponse> {
+        println("title: ${postSearchRequest.title}")
+        println("createdBy: ${postSearchRequest.createdBy}")
+
+        return Page.empty()
+    }
 }
