5045b153910599e232f5d5ab79715996a5538b3f
yklee <gerion@gmail.com>
2023-10-04 22:11:44 +0900

    feat: Add count function for likes

--- src/main/kotlin/com/jake/simpleboard/repository/LikeRepository.kt
+++ src/main/kotlin/com/jake/simpleboard/repository/LikeRepository.kt
@@ -3,4 +3,7 @@ package com.jake.simpleboard.repository
 import com.jake.simpleboard.domain.Like
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LikeRepository : JpaRepository<Like, Long>
+interface LikeRepository : JpaRepository<Like, Long> {
+
+    fun countByPostId(postId: Long): Long
+}
