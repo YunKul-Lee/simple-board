af004a76dd537e57a4038669950b9c241dc265de
jake <gerion@gmail.com>
2023-10-08 19:54:58 +0900

    ktlintFormat

--- src/main/kotlin/com/jake/simpleboard/domain/Comment.kt
+++ src/main/kotlin/com/jake/simpleboard/domain/Comment.kt
@@ -2,7 +2,15 @@ package com.jake.simpleboard.domain
 
 import com.jake.simpleboard.exception.CommentNotUpdatableException
 import com.jake.simpleboard.service.dto.CommentUpdateRequestDto
-import jakarta.persistence.*
+import jakarta.persistence.ConstraintMode
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ForeignKey
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 
 @Entity
 class Comment(
